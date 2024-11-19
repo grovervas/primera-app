9862ac38595d0eb85d714eee94ef69ca78b4020a
Grover Vasquez <grovervas@hotmail.com>
2024-11-18 21:01:51 -0500

    Agregamo Login

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, url_for, abort, session
 from werkzeug.utils import redirect
 
 
 app = Flask(__name__)
+app.secret_key = '50ee6def18a0af7cb5271194656519317732b6867da284e942c0dbfdabe276e4'
 
 @app.route('/')
 def inicio():
-    app.logger.info(f'Entramos al path {request.path}')
-    return 'Hola mundo desde Flask'
+    if 'username' in session:
+        return f'El usuario inicio sesión {session["username"]}'
+    return 'No inicio sesión'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('inicio'))
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('inicio'))
 
 @app.route('/saludar/<nombre>')
 def saludar(nombre):
@@ -25,3 +39,11 @@ def mostrar(valor):
 @app.route('/redireccionar')
 def redireccionar():
     return redirect(url_for('mostrar', valor='Pablo'))
+
+@app.route('/error')
+def error():
+    return abort(404)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html', error=error), 404
